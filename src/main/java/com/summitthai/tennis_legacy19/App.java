2c749127bb11d7f49c2aadd844a28cc39c1e80ad
narindapi <wimonrat22kwan@gmail.com>
2021-04-29 10:39:54 +0700

    Init project

---
+++ src/main/java/com/summitthai/tennis_legacy19/App.java
@@ -0,0 +1,7 @@
+package com.summitthai.tennis_legacy19;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/summitthai/tennis_legacy19/AppTest.java
@@ -0,0 +1,13 @@
+package com.summitthai.tennis_legacy19;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
